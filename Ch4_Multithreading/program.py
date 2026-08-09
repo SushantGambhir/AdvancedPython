13e794baed7d0123eeb9b5682e7aad9dc088ee0b
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-08-09 17:32:20 +0530

    Multithreading

---
+++ Ch4_Multithreading/program.py
@@ -0,0 +1,29 @@
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+def fetch_data(url: str):
+    print("Fetching data from:", url)
+    time.sleep(5)
+    print("Data fetched from:", url)
+    return "Data from " + url
+
+urls_list = ["https://example.com/api/data1", 
+             "https://example.com/api/data2", 
+             "https://example.com/api/data3", 
+             "https://example.com/api/data4", 
+             "https://example.com/api/data5"]
+
+# Traditional brute force way to have this function called for each url defined in the above list, we would do it as follows:
+# for i in urls_list:
+#     fetch_data(i)
+# However what if there are 1000s of URLs and data is big, it takes time to fetch data. It will too slow
+# Hence we shall be using the multithreading
+
+results = []
+with ThreadPoolExecutor(max_workers=len(urls_list)) as executor:
+    futures = executor.map(fetch_data,urls_list)
+# Alternatively the above statement can also be used as:
+#    futures = [executor.submit(fetch_data,url) for url in urls_list]
+    results.extend(futures)
+
+print(results)
