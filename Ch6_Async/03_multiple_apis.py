5d5b73ffaa3ed4cf659a03957c6949c330fdbf70
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-08-16 18:29:09 +0530

    Async

--- Ch6_Async/03_multiple_apis.py
+++ Ch6_Async/03_multiple_apis.py
@@ -1,13 +1,22 @@
 import time
 import asyncio
 
-async def api_call(url: str):
+async def api_call(url: str, delay: int):
     print('Fetching data from: ', url)
-    await asyncio.sleep(3)
+    await asyncio.sleep(delay)
+    print('Data fetched from: ', url)
     return f'{url} data'
 
-async def execute():
-    result = await api_call('orders')
-    print('Data Fetched: ', result)
+async def main():
+    # Creating Tasks with gather
+    tasks = await asyncio.gather(
+        api_call("https://api1.com",2),
+        api_call("https://api2.com",3),
+        api_call("https://api3.com",1)
+    )
+     # Another way to create tasks with list comprehension
+     # tasks = [api_call(url) for url in ['https://api1.com','https://api2.com','https://api3.com']]
+     # results = await asyncio.gather(*tasks)
+    print('All API calls completed')
 
-asyncio.run(execute())
+asyncio.run(main())
